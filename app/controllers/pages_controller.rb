1fd21b9154c8f3f17310751ce1a69234230f7a53
Pramod Sudheendra <pramodks2006@gmail.com>
2011-06-05 03:49:09 -0700

    Added some static stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @contact = "Contact"
   end
 
+  def about
+    @about = "About"
+  end
 end
